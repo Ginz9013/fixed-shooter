c82ff8b85d2bca139a84711e740b446971c8b540
Kouhei <alan829221@gmail.com>
2025-07-18 18:14:40 +0800

    refactor: 新增 useGameManager 處理主要遊戲邏輯，並保持 Game.tsx 簡潔

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,6 +1,5 @@
-import { useEffect, useMemo, useState } from "react";
-import { extend, useApplication } from "@pixi/react";
-import { Container, Text, Ticker } from "pixi.js";
+import { extend } from "@pixi/react";
+import { Container, Text } from "pixi.js";
 
 import Background from "../components/Background";
 import Ghost from "../components/Ghost";
@@ -9,21 +8,11 @@ import CharBullet from "./CharBullet";
 import Heart from "./Heart";
 import GhostBullet from "./GhostBullet";
 
-import { useGhost } from "../hooks/useGhost";
-import { useGhostGroupMovement } from "../hooks/useGhostGroupMovement";
-import { useCharacter } from "../hooks/useCharacter";
-import { useCharBullet } from "../hooks/useCharBullet";
-import { useHearts } from "../hooks/useHearts";
-import { useGhostBullet } from "../hooks/useGhostBullet";
+import { useGameManager } from "../hooks/useGameManager";
 
 import {
   GHOST_GROUP_INIT_X,
   GHOST_GROUP_INIT_Y,
-  LEFT_BOUND,
-  MOVE_SPEED,
-  RIGHT_BOUND,
-  SHOOT_INTERVAL_MS,
-  SHOOTING_PROBABILITY
 } from "../config/game";
 
 
@@ -33,100 +22,30 @@ extend({
 });
 
 const Game = () => {
-  const { app } = useApplication();
-
-  // 血量 & 是否結束遊戲
-  const { hearts, takeDamage } = useHearts();
-
-  // 角色
-  const { charRef, moveDir } = useCharacter();
-  // 角色子彈狀態
-  const { charBullets, handleCharBulletMount, clearCharBullets, onCharFire, updateCharBullets } = useCharBullet();
-
-  // 幽靈群
-  const { ghosts, ghostRefs, handleGhostMount, handleGhostBatchDelete } = useGhost();
-  // 幽靈群組
-  const { ghostGroupRef, bobbing } = useGhostGroupMovement();
-  // 幽靈子彈狀態
-  const { ghostBullets, handleGhostBulletMount, clearGhostBullets, onGhostFire, updateGhostBullets } = useGhostBullet();
-
-  // 計時
-  const [timing, setTiming] = useState<number>(60);
-
-  // 分數
-  const [score, setScore] = useState<number>(0);
-
-  // 遊戲結束
-  const isGameOver = useMemo(() => hearts.length <= 0 || timing <= 0, [hearts, timing]);
-  const isGameCompleted = useMemo(() => ghosts.length <= 0, [ghosts]);
-
-  // 加分方法
-  const addScore = (score: number) => setScore(prev => prev + score); 
-
-  // 倒數計時
-  useEffect(() => {
-    const timer = setInterval(() => {
-      setTiming(prev => prev - 1);
-    }, 1000);
-    return () => clearInterval(timer);
-  }, []);
-
-  // Game Loop
-  useEffect(() => {
-    // 角色每隔 2 秒發射子彈
-    const timer = setInterval(() => {
-      onCharFire(charRef);
-    }, SHOOT_INTERVAL_MS);
-
-    const gameLoop = (ticker: Ticker) => {
-      if(!ghostGroupRef.current || !charRef.current) return;
-
-      // 更新角色位置
-      if (moveDir.current !== 0) {
-        const nextX = Math.max(
-          LEFT_BOUND,
-          Math.min(RIGHT_BOUND, charRef.current.x + MOVE_SPEED * moveDir.current)
-        );
-        charRef.current.x = nextX;
-      };
-
-      // 更新角色子彈
-      updateCharBullets(ticker.deltaTime, ghostRefs, handleGhostBatchDelete, addScore);
-
-      
-      // 控制幽靈群組位置 & 上下漂浮
-      
-      bobbing(ticker.deltaTime);
-
-      // 幽靈根據機率發射子彈
-      ghostRefs.current.forEach((ghost) => {
-        if (Math.random() < SHOOTING_PROBABILITY) {
-          // 子彈初始位置
-          // 幽靈的中心點位置 + 微調修正
-          if (isGameOver) return; // 遊戲結束後不再射擊
-          onGhostFire(ghost);
-        }
-      })
-
-      // 更新幽靈子彈位置
-      updateGhostBullets(ticker.deltaTime, charRef.current, takeDamage);
-      
-    };
-
-    app.ticker.add(gameLoop);
-
-    return () => {
-      app.ticker.remove(gameLoop);
-      clearInterval(timer)
-    };
-  }, [onCharFire]);
-
-  // 結束遊戲 - 清空子彈
-  useEffect(() => {
-    clearGhostBullets();
-    clearCharBullets();
-  }, [isGameOver, isGameCompleted]);
-
+  
+  const {
+    // 愛心
+    hearts,
+    // 分數
+    score,
+    // 倒數計時
+    timing,
+    // 幽靈
+    ghosts,
+    ghostGroupRef,
+    handleGhostMount,
+    // 幽靈子彈
+    ghostBullets,
+    handleGhostBulletMount,
+    // 角色
+    charRef,
+    // 角色子彈
+    charBullets,
+    handleCharBulletMount,
+    // 遊戲狀態
+    isGameOver,
+    isGameCompleted,
+  } = useGameManager();
 
   return (
     <pixiContainer x={0} y={0}>
---
+++ src/hooks/useGameManager.ts
@@ -0,0 +1,151 @@
+import { useCallback, useEffect, useMemo, useState } from "react";
+import { type Ticker } from "pixi.js";
+import { useApplication } from "@pixi/react";
+import { useGhost } from "./useGhost";
+import { useGhostGroupMovement } from "./useGhostGroupMovement";
+import { useCharacter } from "./useCharacter";
+import { useCharBullet } from "./useCharBullet";
+import { useHearts } from "./useHearts";
+import { useGhostBullet } from "./useGhostBullet";
+import { SHOOT_INTERVAL_MS, LEFT_BOUND, RIGHT_BOUND, MOVE_SPEED, SHOOTING_PROBABILITY } from "../config/game";
+
+
+export const useGameManager = () => {
+  const { app } = useApplication();
+
+  // 血量 & 是否結束遊戲
+  const { hearts, takeDamage } = useHearts();
+
+  // 角色
+  const { charRef, moveDir } = useCharacter();
+  // 角色子彈狀態
+  const { charBullets, handleCharBulletMount, clearCharBullets, onCharFire, updateCharBullets } = useCharBullet();
+
+  // 幽靈群
+  const { ghosts, ghostRefs, handleGhostMount, handleGhostBatchDelete } = useGhost();
+  // 幽靈群組
+  const { ghostGroupRef, bobbing } = useGhostGroupMovement();
+  // 幽靈子彈狀態
+  const { ghostBullets, handleGhostBulletMount, clearGhostBullets, onGhostFire, updateGhostBullets } = useGhostBullet();
+
+  // 計時
+  const [timing, setTiming] = useState<number>(60);
+
+  // 分數
+  const [score, setScore] = useState<number>(0);
+
+  // 遊戲結束
+  const isGameOver = useMemo(() => hearts.length <= 0 || timing <= 0, [hearts, timing]);
+  const isGameCompleted = useMemo(() => ghosts.length <= 0, [ghosts]);
+
+  // 加分方法
+  const addScore = useCallback((score: number) => setScore(prev => prev + score), [setScore]);
+
+
+  // Game Loop
+  useEffect(() => {
+    // 當遊戲結束時，清空子彈並停止遊戲
+    if (isGameOver || isGameCompleted) {
+      clearGhostBullets();
+      clearCharBullets();
+      return;
+    };
+
+
+    // 倒數計時
+    const countDownTimer = setInterval(() => {
+      setTiming(prev => prev - 1);
+    }, 1000);
+
+
+    // 角色每隔 2 秒發射子彈
+    const timer = setInterval(() => {
+      onCharFire(charRef);
+    }, SHOOT_INTERVAL_MS);
+
+
+    // 主要的遊戲循環
+    const gameLoop = (ticker: Ticker) => {
+      if(!ghostGroupRef.current || !charRef.current) return;
+
+      // 更新角色位置
+      if (moveDir.current !== 0) {
+        const nextX = Math.max(
+          LEFT_BOUND,
+          Math.min(RIGHT_BOUND, charRef.current.x + MOVE_SPEED * moveDir.current)
+        );
+        charRef.current.x = nextX;
+      };
+
+      // 更新角色子彈
+      updateCharBullets(ticker.deltaTime, ghostRefs, handleGhostBatchDelete, addScore);
+
+      
+      // 控制幽靈群組位置 & 上下漂浮
+      
+      bobbing(ticker.deltaTime);
+
+      // 幽靈根據機率發射子彈
+      ghostRefs.current.forEach((ghost) => {
+        if (Math.random() < SHOOTING_PROBABILITY) {
+          // 子彈初始位置
+          // 幽靈的中心點位置 + 微調修正
+          if (isGameOver) return; // 遊戲結束後不再射擊
+          onGhostFire(ghost);
+        }
+      })
+
+      // 更新幽靈子彈位置
+      updateGhostBullets(ticker.deltaTime, charRef.current, takeDamage);
+    };
+
+    app.ticker.add(gameLoop);
+
+    return () => {
+      app.ticker.remove(gameLoop);
+      clearInterval(timer);
+      clearInterval(countDownTimer);
+    };
+  }, [
+    app.ticker,
+    charRef,
+    moveDir,
+    onCharFire,
+    updateCharBullets,
+    ghostRefs,
+    bobbing,
+    onGhostFire,
+    updateGhostBullets,
+    handleGhostBatchDelete,
+    takeDamage,
+    addScore,
+    clearGhostBullets,
+    clearCharBullets,
+    isGameCompleted,
+    isGameOver,
+  ]);
+
+  return {
+    // 愛心
+    hearts,
+    // 分數
+    score,
+    // 倒數計時
+    timing,
+    // 幽靈
+    ghosts,
+    ghostGroupRef,
+    handleGhostMount,
+    // 幽靈子彈
+    ghostBullets,
+    handleGhostBulletMount,
+    // 角色
+    charRef,
+    // 角色子彈
+    charBullets,
+    handleCharBulletMount,
+    // 遊戲狀態
+    isGameOver,
+    isGameCompleted,
+  };
+}
